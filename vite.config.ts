8e4914d3143c922285f03d13b9a5c2a74d9889bf
JozefTkocz <joe@bitbloom.tech>
2025-12-16 23:14:17 +0000

    set base in CI

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.CI ? '/tumpr-web-app/' : '/',
   plugins: [
     tanstackRouter({
       target: 'react',
