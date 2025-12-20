ca0a2d893655fc1b6ab0c8d0801b6e8c3b59473d
JozefTkocz <joe@bitbloom.tech>
2025-12-20 23:26:57 +0000

    session storage for location cache

--- src/hooks/location.tsx
+++ src/hooks/location.tsx
@@ -1,26 +1,34 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 export type Coordinate = {
-  latitude: number;
-  longitude: number;
-  altitude: number | null;
-};
+  latitude: number
+  longitude: number
+  altitude: number | null
+}
 
 export function useLocation() {
-  const [location, setLocation] = useState<Coordinate>();
+  const cachedLocationString = globalThis.sessionStorage.getItem('location')
+  const cachedLocation = cachedLocationString
+    ? (JSON.parse(cachedLocationString) as Coordinate)
+    : null
+  const [location, setLocation] = useState<Coordinate | null>(cachedLocation)
+  const setLocationAndUpdateCache = (coordinate: Coordinate) => {
+    setLocation(coordinate)
+    globalThis.sessionStorage.setItem('location', JSON.stringify(coordinate))
+  }
 
   const id = navigator.geolocation.watchPosition(
     (pos) =>
-      setLocation({
+      setLocationAndUpdateCache({
         longitude: pos.coords.longitude,
         latitude: pos.coords.latitude,
         altitude: pos.coords.altitude,
       }),
     (err) => {
-      console.log(err);
+      console.log(err)
     },
     { enableHighAccuracy: true },
-  );
+  )
 
-  return { location, id };
+  return { location, id }
 }
